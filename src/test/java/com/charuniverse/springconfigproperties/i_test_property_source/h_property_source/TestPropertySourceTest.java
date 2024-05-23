30b3a82a79f6c08a6be22b39e69cdb28ae3c784c
ahps <akbarputrasrg@gmail.com>
2024-05-23 11:50:28 +0700

    course i: test property source

---
+++ src/test/java/com/charuniverse/springconfigproperties/i_test_property_source/h_property_source/TestPropertySourceTest.java
@@ -0,0 +1,43 @@
+package com.charuniverse.springconfigproperties.i_test_property_source.h_property_source;
+
+import lombok.Getter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
+
+@TestPropertySources({
+        @TestPropertySource("classpath:/test.properties")
+})
+@SpringBootTest(classes = TestPropertySourceTest.TestApplication.class)
+public class TestPropertySourceTest {
+
+    @Autowired
+    private TestApplication.SampleProperties sampleProperties;
+
+    @Test
+    void testTestPropertySource() {
+        Assertions.assertEquals("sample gemink test", sampleProperties.getName());
+        Assertions.assertEquals(1, sampleProperties.getVersion());
+    }
+
+    @SpringBootApplication
+    public static class TestApplication {
+
+        @Getter
+        @Component
+        public static class SampleProperties {
+
+            @Value("${sample.name}")
+            private String name;
+
+            @Value("${sample.version}")
+            private Integer version;
+        }
+    }
+}
